d95d837decdb3ed8d2344a5d4d5833641cd15401
JocimarGalante <jocimarcgalante@hotmail.com>
2024-04-17 18:13:50 -0300

    Atualização

---
+++ parameters.py
@@ -0,0 +1,6 @@
+file_name = 'connections.csv'
+linkedin_username = 'user@gmail.com'
+linkedin_password = 'password'
+keywords = 'DevOps,Linux,Python,AWS'
+ignore_list = 'Person1,Person2'
+till_page = 10
